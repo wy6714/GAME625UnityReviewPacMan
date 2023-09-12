c850a65cff11e92851370ddf1328d7c4c95248d4
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-09-12 18:15:00 -0400

    freze and death

--- UnityReviewPacMan/Assets/-Script/Player.cs
+++ UnityReviewPacMan/Assets/-Script/Player.cs
@@ -5,14 +5,32 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+
+    private Vector3 targetPosition = new Vector3(1.4f, 0f, -16f);
+    private Vector3 hidePosition = new Vector3(100f, 0f, 100f);
+
     private CharacterController characterController;
+
+    private float frezeDuration = 5f;
+    private float deathDuration = 2f;
+
     [SerializeField]private float speed = 10f;
-    
+
+    private int score = 0;
+    private int pelletPoint = 10;
+    private int powerUpPelletPoint = 100;
+    private int lives = 3;
+
+    private bool enemyFrozen = false;
+
+    //private Vector3 originalScale;
     
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        
+       
     }
 
     // Update is called once per frame
@@ -41,4 +59,59 @@ public class Player : MonoBehaviour
 
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("pellet"))//player get pellet
+        {
+            Destroy(other.gameObject);
+            score += pelletPoint;
+            Debug.Log(score);
+        }
+
+        if (other.CompareTag("powerUpPellet"))//player get power up pellet
+        {
+            Destroy(other.gameObject);
+            score += powerUpPelletPoint;
+            StartCoroutine(FreezeForDuration());
+
+            Debug.Log(score);
+        }
+
+        if (other.CompareTag("enemy") && !enemyFrozen) //player eaten by enemy
+        {
+            StartCoroutine(playerDeath());
+            lives -= 1;
+            Debug.Log("lives -1");
+        }
+        else if (other.CompareTag("enemy") && enemyFrozen)//enemy eaten by player
+        {
+            Destroy(other.gameObject);
+            score += 200;
+            Debug.Log("eat enemy");
+        }
+
+    }
+
+    private IEnumerator FreezeForDuration()
+    {
+        enemyFrozen = true;
+        yield return new WaitForSeconds(frezeDuration);
+        enemyFrozen = false;
+
+
+    }
+    private IEnumerator playerDeath()
+    {
+        yield return new WaitForSeconds(0.01f);
+        transform.position = hidePosition;
+        
+        yield return new WaitForSeconds(deathDuration);
+        
+        transform.position = targetPosition;
+        
+    }
+
+
+
 }
